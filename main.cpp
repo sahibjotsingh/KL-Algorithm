bedea68652abf1f5bca6f0eb7fec8027dbf42a51
Sahibjot Singh Bedi <sahibjot.bedi230@gmail.com>
2018-02-11 20:00:24 +0530

    Changes

--- main.cpp
+++ main.cpp
@@ -12,22 +12,19 @@ void swap(vector<int> &partition1, vector<int> &partition2, int pos1, int pos2)
 	partition2[pos2] = temp;
 }
 
-void KLAlgo(map< pair<int, int>, int> &edgeSet, vector<int> &ans, vector<bool> &permanent_lock, vector<int> &D, int K1, int nodes)
-{
-	if(K1==1)
-		return;
-		
+void KLAlgo(map< pair<int, int>, int> &edgeSet, vector<int> &ans, vector<int> &permanent_lock, vector<int> &D, int K1, int nodes, int inodes)
+{	
 	int i, j, k;
-	int s1=nodes-(nodes/K1), s2=nodes/K1;
+	int s1 = inodes-(inodes/K1), s2 = inodes/K1;
 	vector<int> partition1, partition2;
-	vector<bool> lock(nodes, false);		
+	vector<int> lock(nodes, false);		
 	Partition p(nodes);
 	DValue objD(nodes);
 
 	p.partition(partition1, partition2, s1, s2, permanent_lock);
 	objD.initializeDValue(D);
 	objD.findDValue(D, edgeSet, partition1, partition2);
-	
+
 	int g, ig;
 	int pnode1, pnode2;
 	vector<int> gval;
@@ -83,8 +80,8 @@ void KLAlgo(map< pair<int, int>, int> &edgeSet, vector<int> &ans, vector<bool> &
 
 		objD.changeDValue(D, edgeSet, partition1[pnode1], partition2[pnode2], partition1, partition2);
 	}
-		
-	int maxg=gval[0], imaxg=gval[0], K=0;
+	
+	int maxg=gval[0], imaxg=gval[0], K=1;
 	for( i=1; i < gval.size(); ++i)
 	{
 		imaxg = imaxg+gval[i];
@@ -94,32 +91,35 @@ void KLAlgo(map< pair<int, int>, int> &edgeSet, vector<int> &ans, vector<bool> &
 			K = i;
 		}
 	}
-	//cout<<endl<<endl<<K<<endl;
 		
-	if(maxg<0 && K==0)
+	if(maxg<0 && K==1)
 		K=-1;
 		
 	for(i=0;i<K;++i)
 	{
 		swap(partition1, partition2, pos1[i], pos2[i]);
 	}
-
+	
 	if((K1-1) == 1)
 	{
 		for(i=0;i<partition1.size();++i)
 		{
 			ans.push_back(partition1[i]);
+			cout<<partition1[i]<<" ";
+			permanent_lock[partition1[i]]=1;
 		}
+		cout<<endl;
 		ans.push_back(-1);
 	}
 
 	for(i=0;i<partition2.size();++i)
 	{
 		ans.push_back(partition2[i]);
+		cout<<partition2[i]<<" ";
+		permanent_lock[partition2[i]]=1;
 	}
+	cout<<endl;
 	ans.push_back(-1);
-
-	KLAlgo(edgeSet, ans, permanent_lock, D, K1-1, s1);
 }
 
 int main()
@@ -134,20 +134,35 @@ int main()
 
 	Graph graph; 
 	nodes = graph.Nodes(); 
-	graph.buildEdgeSet(edgeSet);
-
-	vector<bool> permanent_lock(nodes, false);
-	vector<int> D(nodes, 0);
-	
-	KLAlgo(edgeSet, ans, permanent_lock, D, K1, nodes);
-
-	cout<<endl<<"The partitions are separated by newline"<<endl;
-	for (i = 0; i < ans.size(); i++)
+	int inodes = nodes;
+	if(nodes == K1)
 	{
-    	if(ans[i]!=-1)
-    		cout<<ans[i]<<" ";
-    	else
-    		cout<<endl;
+		cout<<"Given graph is already divided into "<<K1<<" partitions"<<endl;
+	}
+	else
+	{
+		graph.buildEdgeSet(edgeSet);
+
+		vector<int> permanent_lock(nodes, false);
+		vector<int> D(nodes, 0);
+	
+		while(K1!=1)
+		{
+			KLAlgo(edgeSet, ans, permanent_lock, D, K1, nodes, inodes);
+			inodes = inodes - (inodes/K1);
+			--K1;
+		}
+		
+		/*
+		cout<<endl<<"The partitions are separated by newline"<<endl;
+		for (i = 0; i < ans.size(); i++)
+		{ 
+	    	if(ans[i]!=-1)
+	    		cout<<ans[i]<<" ";
+	    	else
+	    		cout<<endl;
+		}
+		*/
 	}
 	return 0;
 }
