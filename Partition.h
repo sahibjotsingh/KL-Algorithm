ef02684ada58ca7c879ec9ef741c1007146031b6
Sahibjot Singh Bedi <sahibjot.bedi230@gmail.com>
2018-02-11 20:01:03 +0530

    Changes

--- Partition.h
+++ Partition.h
@@ -8,7 +8,7 @@ class Partition
 	int nodes;
 	public:
 		Partition(int nodes);
-		void partition(vector<int> &partition1, vector<int> &partition2, int s1, int s2, vector<bool> &permanent_lock);
+		void partition(vector<int> &partition1, vector<int> &partition2, int s1, int s2, vector<int> &permanent_lock);
 };
 
 #endif
